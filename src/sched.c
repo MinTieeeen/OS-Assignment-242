a7c47856d3bff5a93b917956a9f75d62191c2d56
Dũng Nguyễn <dung.nguyenbk2310561@hcmut.edu.vn>
2025-04-24 18:40:13 +0700

    Revert "Đã test sched"

--- src/sched.c
+++ src/sched.c
@@ -1,4 +1,5 @@
- #include "queue.h"
+
+#include "queue.h"
 #include "sched.h"
 #include <pthread.h>
 
@@ -30,12 +31,11 @@ void init_scheduler(void) {
 
 	for (i = 0; i < MAX_PRIO; i ++) {
 		mlq_ready_queue[i].size = 0;
-		slot[i] = MAX_PRIO - i;
+		slot[i] = MAX_PRIO - i; 
 	}
 #endif
 	ready_queue.size = 0;
 	run_queue.size = 0;
-
 	pthread_mutex_init(&queue_lock, NULL);
 }
 
@@ -46,35 +46,12 @@ void init_scheduler(void) {
  *  We implement stateful here using transition technique
  *  State representation   prio = 0 .. MAX_PRIO, curr_slot = 0..(MAX_PRIO - prio)
  */
-
 struct pcb_t * get_mlq_proc(void) {
 	struct pcb_t * proc = NULL;
 	/*TODO: get a process from PRIORITY [ready_queue].
 	 * Remember to use lock to protect the queue.
 	 * */
-	
-	for (int i=0; i<MAX_PRIO; i++) {
-		if (slot[i] > 0 && !empty(&mlq_ready_queue[i])) {
-		// if this ready queue isn't empty and it hasn't used all slots
-			pthread_mutex_lock(&queue_lock);
-			proc = dequeue(&mlq_ready_queue[i]);
-			if (proc != NULL) {
-				enqueue(&run_queue, proc); // put it into running queue!
-				slot[i]--; // used up 1 slot
-			}			
-			pthread_mutex_unlock(&queue_lock);
-			break;
-		}
-	}
-
-	if (!proc) {
-	// either no process is ready, or all queues used up their slots
-	// we'll reset slots and try again later
-	for (int i=0; i<MAX_PRIO; i++)
-		slot[i] = MAX_PRIO - i;
-	}
-	
-	return proc;
+	return proc;	
 }
 
 void put_mlq_proc(struct pcb_t * proc) {
@@ -96,12 +73,9 @@ struct pcb_t * get_proc(void) {
 void put_proc(struct pcb_t * proc) {
 	proc->ready_queue = &ready_queue;
 	proc->mlq_ready_queue = mlq_ready_queue;
-	proc->running_list = &running_list;
+	proc->running_list = & running_list;
 
 	/* TODO: put running proc to running_list */
-	pthread_mutex_lock(&queue_lock);
-	enqueue(&running_list, proc);
-	pthread_mutex_unlock(&queue_lock);
 
 
 	return put_mlq_proc(proc);
@@ -113,9 +87,6 @@ void add_proc(struct pcb_t * proc) {
 	proc->running_list = & running_list;
 
 	/* TODO: put running proc to running_list */
-	pthread_mutex_lock(&queue_lock);
-	enqueue(&running_list, proc);
-	pthread_mutex_unlock(&queue_lock);
 
 	return add_mlq_proc(proc);
 }
@@ -125,9 +96,6 @@ struct pcb_t * get_proc(void) {
 	/*TODO: get a process from [ready_queue].
 	 * Remember to use lock to protect the queue.
 	 * */
-	pthread_mutex_lock(&queue_lock);
-	proc = dequeue(&ready_queue);
-	pthread_mutex_unlock(&queue_lock);
 	return proc;
 }
 
@@ -136,9 +104,8 @@ void put_proc(struct pcb_t * proc) {
 	proc->running_list = & running_list;
 
 	/* TODO: put running proc to running_list */
-	pthread_mutex_lock(&queue_lock);
-	enqueue(&running_list, proc);
 
+	pthread_mutex_lock(&queue_lock);
 	enqueue(&run_queue, proc);
 	pthread_mutex_unlock(&queue_lock);
 }
@@ -148,9 +115,8 @@ void add_proc(struct pcb_t * proc) {
 	proc->running_list = & running_list;
 
 	/* TODO: put running proc to running_list */
-	pthread_mutex_lock(&queue_lock);
-	enqueue(&running_list, proc);
 
+	pthread_mutex_lock(&queue_lock);
 	enqueue(&ready_queue, proc);
 	pthread_mutex_unlock(&queue_lock);	
 }
--- src/sys_killall.c
+++ src/sys_killall.c
@@ -13,9 +13,6 @@
 #include "stdio.h"
 #include "libmem.h"
 
-#include "queue.h"
-#include <string.h>
-
 int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
 {
     char proc_name[100];
@@ -41,27 +38,7 @@ int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
      */
     //caller->running_list
     //caller->mlq_ready_queu
-    int running_size = caller->running_list->size;
-    struct pcb_t *cur_proc;
-    for (int j=0; j<running_size; j++) {
-        cur_proc = caller->running_list->proc[j];
-        if (strcmp(cur_proc->path, proc_name) == 0) {
-            // free this proc, then push all later proc forward
-            
-            // free this proc's memory
-            for (int k=0; k<PAGING_MAX_SYMTBL_SZ; k++) {
-                while (cur_proc->mm->symrgtbl[k].rg_next != NULL) {
-                    libfree(caller, k);
-                }
-            }
-            // now shift all procs forward
-            for (int k=j; k<running_size-1; k++) {
-                caller->running_list->proc[k] = caller->running_list->proc[k+1];
-            }
-            // and pop the redundant proc out
-            --caller->running_list->size;
-        }
-    }
+
     /* TODO Maching and terminating 
      *       all processes with given
      *        name in var proc_name
